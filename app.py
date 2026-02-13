34b0d35b00b55989f8dde1fdfb17729836c29945
bhavyabom12-glitch <bhavyabom12@gmail.com>
2026-02-13 13:31:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1090,10 +1090,6 @@ FEED_HTML = '''
 </html>
 '''
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
-
 @app.route('/admin')
 def admin():
     """Admin dashboard - shows research stats"""
@@ -1199,3 +1195,7 @@ def admin():
     '''
     
     return html
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
